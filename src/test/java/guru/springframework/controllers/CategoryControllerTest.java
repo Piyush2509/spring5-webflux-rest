556619a3dcab36bde6528c33e1e410e973d56fef
Piyush2509 <piyush.jayaswal@gmail.com>
2018-08-22 15:40:55 -0400

    Update category using patch

--- src/test/java/guru/springframework/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/controllers/CategoryControllerTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -32,15 +35,15 @@ public class CategoryControllerTest {
 
 	@Test
 	public void testList() throws Exception {
-		BDDMockito.given(categoryRepository.findAll()).willReturn(Flux
-				.just(Category.builder().description("Cat1").build(), Category.builder().description("Cat2").build()));
+		given(categoryRepository.findAll()).willReturn(Flux.just(Category.builder().description("Cat1").build(),
+				Category.builder().description("Cat2").build()));
 
 		webTestClient.get().uri("/api/v1/categories").exchange().expectBodyList(Category.class).hasSize(2);
 	}
 
 	@Test
 	public void testGetById() throws Exception {
-		BDDMockito.given(categoryRepository.findById("someid"))
+		given(categoryRepository.findById("someid"))
 				.willReturn(Mono.just(Category.builder().description("Cat1").build()));
 
 		webTestClient.get().uri("/api/v1/categories/someid").exchange().expectBodyList(Category.class);
@@ -49,7 +52,7 @@ public class CategoryControllerTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testCreate() throws Exception {
-		BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+		given(categoryRepository.saveAll(any(Publisher.class)))
 				.willReturn(Flux.just(Category.builder().description("descrp").build()));
 
 		Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Some Cat").build());
@@ -60,13 +63,40 @@ public class CategoryControllerTest {
 
 	@Test
 	public void testUpdate() throws Exception {
-		BDDMockito.given(categoryRepository.save(any(Category.class)))
-				.willReturn(Mono.just(Category.builder().build()));
+		given(categoryRepository.save(any(Category.class))).willReturn(Mono.just(Category.builder().build()));
 
 		Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
 
 		webTestClient.put().uri("/api/v1/categories/someid").body(catToUpdateMono, Category.class).exchange()
 				.expectStatus().isOk();
+	}
+
+	@Test
+	public void testPatch() throws Exception {
+		given(categoryRepository.findById(anyString())).willReturn(Mono.just(Category.builder().build()));
+
+		given(categoryRepository.save(any(Category.class))).willReturn(Mono.just(Category.builder().build()));
+
+		Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
+
+		webTestClient.patch().uri("/api/v1/categories/someid").body(catToUpdateMono, Category.class).exchange()
+				.expectStatus().isOk();
+
+		verify(categoryRepository).save(any());
+	}
+
+	@Test
+	public void testPatchNoChanges() throws Exception {
+		given(categoryRepository.findById(anyString())).willReturn(Mono.just(Category.builder().build()));
+
+		given(categoryRepository.save(any(Category.class))).willReturn(Mono.just(Category.builder().build()));
+
+		Mono<Category> catToUpdateMono = Mono.just(Category.builder().build());
+
+		webTestClient.patch().uri("/api/v1/categories/someid").body(catToUpdateMono, Category.class).exchange()
+				.expectStatus().isOk();
+
+		verify(categoryRepository, never()).save(any());
 	}
 
 }
